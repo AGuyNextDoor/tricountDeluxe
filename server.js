181810a4d23e2e97666efeecaa2c9be4cf3a041c
Martin Vielvoye <martin.vielvoye@gmail.com>
2018-05-15 17:59:16 +0200

    solved problem for cookies

--- server.js
+++ server.js
@@ -7,8 +7,68 @@ const nunjucks = require("nunjucks");
 
 const port = process.env.PORT || 3000;
 const client = new PG.Client();
-
 client.connect();
+
+// Initialize Passport and restore authentication state,
+// if any, from the session.
+
+
+passport.use(
+  new LocalStrategy(function(email, password, callback) {
+    // Here we need to ask the database if a user match with these email and password.
+    // If there is, we can call the callback function with our user object and no errors:
+    //   `callback(null, userObject)`
+    // Or, if we don't find any matching user, call the callback function with just an error:
+    //   `callback(new Error("no user found"))`
+    // User.findOne({ user: username }, function (err, user) {
+      // if (err) { return callback(err); }
+      // if (!user) { return callback(null, false); }
+      // if (!user.verifyPassword(password)) { return callback(null, false); }
+      client.query(
+        "select * from users where nom_user = $1 and pwd_user = $2",
+        [email,password],
+        function(error, resultfunc) {
+          if (error) {
+            console.log("nope");
+          } else {
+            console.log(resultfunc.rows.length);
+            if(resultfunc.rows.length === 0){
+              console.log("nope nope");
+              return callback(null, false);
+            }
+          }
+        }
+      );
+
+      console.log("third line "+ email +" "+ password);
+
+      let user = {
+        "email":`email`,
+        "password":`password`
+      };
+      callback(null,user);
+    // });
+  })
+);
+
+passport.serializeUser(function(user, callback) {
+  //ici on créé le cookie avec les informations : le username;
+  callback(null, user.email);
+});
+
+passport.deserializeUser(function(email, callback) {
+    // Depuis le "username" present dans le cookie, on l'utilise pour recuperer dans la database toutes les informations
+    // du compte pour l'utiliser sur la page internet.
+  //   User.findByEmail(email, function (err, user) {
+  //     callback(err, user);
+  // });
+  user = {
+    "email":"name",
+    "password":"pass"
+  };
+  callback(null, user)
+})
+
 
 nunjucks.configure("views", {
   autoescape: true,
@@ -28,8 +88,6 @@ app.use(
   })
 );
 
-// Initialize Passport and restore authentication state,
-// if any, from the session.
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -37,45 +95,10 @@ app.get("/register", function(request, result) {
   result.render("register");
 });
 
-
-app.get("/user/:username", function (request, result) {
-
-  client.query(
-    "select * from users where nom_user = $1",
-    [request.params.username],
-    function(error, resultfunc) {
-      if (error) {
-        console.log(error);
-      } else {
-        //console.log(resultfunc.rows);
-        result.json(resultfunc.rows);
-      }
-    });
-});
-
-app.post("/register", function(request, result) {
-  const user = request.body;
-  passport.serializeUser(function(user, callback) {
-    //ici on créé le cookie avec les informations : le username;
-    return callback(null, user.email);
-  });
-  // db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [user.email, user.password])
-  // // .then(dbResult => {
-  // //   request.logIn(dbResult[0], function(error) {
-  // //     if (error) {
-  // //       console.log(error);
-  // //       return result.redirect("/register");
-  // //     }
-  // //     return result.redirect("/");
-  // //   });
-  // // })
-  //
-  // .catch(error => {
-  //   console.warn(error);
-  // });
-  console.log(`the entered mail is ${user.username} & the password is ${user.password}`);
+app.post("/register", passport.authenticate('local', {failureRedirect: '/register' }),function(request, result) {
+                                     const user = request.body;
+  //console.log(`the entered mail is ${user.username} & the password is ${user.password}`);
   result.redirect("/");
-
 });
 
 // passport.serializeUser(function(user, callback) {
@@ -88,27 +111,25 @@ app.post("/register", function(request, result) {
 //   // du compte pour l'utiliser sur la page internet.
 // });
 
-passport.use(
-  new LocalStrategy(function(email, password, callback) {
-    // Here we need to ask the database if a user match with these email and password.
-    // If there is, we can call the callback function with our user object and no errors:
-    //   `callback(null, userObject)`
-    // Or, if we don't find any matching user, call the callback function with just an error:
-    //   `callback(new Error("no user found"))`
-  })
-);
-
-app.get("/login", function(request, result) {
-  result.render("login");
-});
-
-app.post(
-  "/login",
-  passport.authenticate("local", { failureRedirect: "/login" }),
-  function(request, result) {
-    result.redirect("/profile");
-  }
-);
+// app.get("/login", function(request, result) {
+//   result.render("login");
+// });
+//
+// app.post("/login", passport.authenticate('local', function(request, result) {
+//   const user = request.body;
+//   client.query(
+//     "select * from users where nom_user = $1 and pwd_user = $2",
+//     [user.username,user.password],
+//     function(error, resultfunc) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log(resultfunc.rows.length);
+//       }
+//     });
+//   //console.log(`the entered mail is ${user.username} & the password is ${user.password}`);
+//   //result.redirect("/");
+// });
 
 app.get("/logout", function(request, result) {
   request.logout();
@@ -116,13 +137,8 @@ app.get("/logout", function(request, result) {
 });
 
 app.get("/", function(request, result){
-  console.log("last log is :", passport.deserializeUser(function(email, callback) {
-      // Depuis le "username" present dans le cookie, on l'utilise pour recuperer dans la database toutes les informations
-      // du compte pour l'utiliser sur la page internet.
-      return email;
-    })
-  );
-  console.log(app.session.passport.user);
+  console.log("last log is :" + request.user.email + " " + request.user + " " + request.user.password);
+  // console.log(app.session.passport.user);
   result.render("homepage");
 });
 
