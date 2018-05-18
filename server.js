57f85cdf26284611adceb59890d2f0999b71da15
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-05-18 15:06:00 +0200

    fixed delete expense

--- server.js
+++ server.js
@@ -231,9 +231,9 @@ app.post("/register", function(request, result) {
               myGraph_data.push(element.sum/100);
               myGraph_labels.push(element.name_transaction);
             });
-let thereIsData = (resultfunc.rows!==undefined);
-let empty = (resultfunc.rows===undefined);
-            result.render("expenses", {user:request.user,tid:thereIsData, empty:empty, test:resultfunc.rows, graph_data:myGraph_data,graph_labels:myGraph_labels, activityId: request.params.id});
+          let thereIsData = (resultfunc.rows!==undefined);
+
+            result.render("expenses", {user:request.user,tid:thereIsData,test:resultfunc.rows, graph_data:myGraph_data,graph_labels:myGraph_labels, activityId: request.params.id});
           }
         }
       );
@@ -287,7 +287,7 @@ app.get("/balance/:actId", function(request,result){
   });
 });
 
-    app.get("/expense/delete/:id", function(request,result){
+    app.get("/expense/delete/:id/:numActivity", function(request,result){
       client.query(
         "DELETE FROM transaction_detail WHERE num_transaction = $1 ;",
         [request.params.id],
@@ -304,7 +304,7 @@ app.get("/balance/:actId", function(request,result){
                   console.warn(error);
                 }else{
 
-                result.redirect("/activity/"+request.params.id+"/expenses");
+                result.redirect("/activity/"+request.params.numActivity+"/expenses");
 
                 }
               }
